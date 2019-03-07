274362ca53ffe89f7362a28b053587a24a9cf252
myeongs14 <audtjr9514@gmail.com>
2019-03-07 12:28:36 +0900

    19.03.07

--- project/settings.py
+++ project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'n^bq4f5r&6$dh3oy!ojmnb0)3!@d*h
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
